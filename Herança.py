c2464b2d0f61104496edf84d85deca212f95db2d
Julianaferreirag <jujufreire@gmail.com>
2023-11-03 12:18:40 -0300

    Initial commit

---
+++ Herança.py
@@ -0,0 +1,22 @@
+from BibliotecaHerança import*
+
+gato=Gato('Luan', 'Preto e Branco' )
+gato.emitirsom()
+gato.comer()
+print()
+
+cachorro=Cachorro('Toy', 'Branco')
+cachorro.emitirsom()
+cachorro.comer()
+print()
+
+coelho=Coelho('Bolinha', 'Petro')
+coelho.emitirsom()
+coelho.comer()
+print()
+
+vaca=Vaca('Bianka', 'Preta e branca')
+vaca.emitirsom()
+vaca.comer()
+print()
+
